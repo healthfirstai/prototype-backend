d23fc7eec53308a3dcffa3003b634722255e5e76
Lawrence Lim <ll4715@nyu.edu>
2023-07-21 11:27:08 +0800

    feat: Added test-json agent and get-plan cli commands

--- healthfirstai_prototype/cli.py
+++ healthfirstai_prototype/cli.py
@@ -1,10 +1,5 @@
-# NOTE: This is an example of how to interact with our application code with the command line
-
-from healthfirstai_prototype.redis_db import (
-    search_similar_food,
-)
-
 from healthfirstai_prototype.generate import get_user_meal_plans
+from healthfirstai_prototype.nutrition_agent import start_nutrition_agent
 from healthfirstai_prototype.transform import (
     delete_all_vectors,
     get_all_foods,
@@ -25,8 +20,37 @@ def get_meal_plan():
     Get a meal plan from the database
     """
     click.echo("Getting meal plan")
-    meal_plan = get_user_meal_plans(1) # NOTE: We are getting the meal plan for user 1
+    meal_plan = get_user_meal_plans(1)  # NOTE: We are getting the meal plan for user 1
     click.echo(meal_plan)
+    click.echo("Finished search")
+
+
+@cli.command()
+def test_json_agent():
+    """
+    Tests the JSON agent to answer questions about a meal plan
+    """
+    click.echo("Getting meal plan")
+    meal_plan_json = get_user_meal_plans(1)
+    click.echo("Starting agent")
+    agent = start_nutrition_agent(meal_plan_json)
+    click.echo("Running agent")
+
+    user_input = "What are the ingredients in my dinner?"
+    click.echo(f"User Input: {user_input}")
+    output = agent.run(user_input)
+    click.echo(f"Output: {output}")
+
+    user_input = "What is the protein content of my dinner?"  # NOTE: JSON currently does not contain macro + micro values
+    click.echo(f"User Input: {user_input}")
+    output = agent.run(user_input)
+    click.echo(f"Output: {output}")
+
+    user_input = "Please edit the above JSON object so that you replace the protein bar with something less sweet but equally protein rich"  # NOTE: Limited to reading the JSON file. We will likely go for another agent
+    click.echo(f"User Input: {user_input}")
+    output = agent.run(user_input)
+    click.echo(f"Output: {output}")
+
     click.echo("Finished search")
 
 
