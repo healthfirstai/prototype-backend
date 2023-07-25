6640f402a69312911d11853dec3518bef49f0932
Lawrence Lim <ll4715@nyu.edu>
2023-07-25 20:55:19 +0800

    Added new function to run JSON chain

--- healthfirstai_prototype/nutrition_chains.py
+++ healthfirstai_prototype/nutrition_chains.py
@@ -3,8 +3,11 @@ from langchain import PromptTemplate, OpenAI, LLMChain
 from healthfirstai_prototype.util_models import ModelName
 from healthfirstai_prototype.utils import get_model
 from healthfirstai_prototype.nutrition_templates import EDIT_JSON_TEMPLATE
+from healthfirstai_prototype.nutrition_utils import get_cached_plan_json, store_new_diet_plan
+import redis
 
 
+# TODO: This is WAY too slow. I need it to only replace certain keys
 def init_edit_json_chain():
     """
     Initialize the edit json chain
@@ -17,3 +20,13 @@ def init_edit_json_chain():
         ),
         verbose=True,
     )
+
+
+def run_edit_json_chain(agent_input: str, user_id: int):
+    user_diet_plan_json = get_cached_plan_json(user_id)
+    new_diet_plan = init_edit_json_chain().predict(
+        agent_input=agent_input,
+        user_diet_plan_json=user_diet_plan_json,
+    )
+    store_new_diet_plan(user_id, new_diet_plan)
+    return "Successfully updated your diet plan!"
