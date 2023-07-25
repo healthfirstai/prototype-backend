e9ef210a2938acac748f0e29471f75482e3f3f78
Lawrence Lim <ll4715@nyu.edu>
2023-07-25 20:56:18 +0800

    Updated utils to add get_embedding_model

--- healthfirstai_prototype/utils.py
+++ healthfirstai_prototype/utils.py
@@ -1,7 +1,7 @@
-# NOTE: Any functions that are used in multiple files should be placed here
 from langchain.chat_models import ChatOpenAI
 from langchain import OpenAI
 from healthfirstai_prototype.util_models import ModelName
+from langchain.embeddings import OpenAIEmbeddings
 
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
@@ -34,6 +34,16 @@ def get_model(model_name: str) -> ChatOpenAI | OpenAI:
             temperature=0,
             streaming=True,
             callbacks=[StreamingStdOutCallbackHandler()],
+        )
+    else:
+        raise ValueError("Model name not recognized")
+
+def get_embedding_model(model_name: str):
+    if model_name == ModelName.text_embedding_ada_002:
+        return OpenAIEmbeddings(
+            client=None,
+            model=ModelName.text_embedding_ada_002,
+            chunk_size=1000,
         )
     else:
         raise ValueError("Model name not recognized")
