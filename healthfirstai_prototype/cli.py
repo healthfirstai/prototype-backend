a9d50ce677763f5903c1a8854c124ef887535b70
Lawrence Lim <ll4715@nyu.edu>
2023-07-25 16:28:47 +0800

    Added get meal to cli

--- healthfirstai_prototype/cli.py
+++ healthfirstai_prototype/cli.py
@@ -1,7 +1,10 @@
 from healthfirstai_prototype.nutrition_utils import (
     get_user_meal_plans_as_json,
     get_user_info_for_json_agent,
+    get_user_meal_info_json,
     get_user_info_dict,
+    store_diet_plan,
+    get_cached_plan_json,
 )
 from healthfirstai_prototype.nutrition_agent import (
     start_nutrition_temp_agent,
@@ -34,13 +37,53 @@ def cli():
     default=True,
     help="Whether to include the ingredients in the meal plan",
 )
-def user_diet_plan(uid: int, include_ingredients: bool):
+def get_diet_plan(uid: int, include_ingredients: bool):
     """
     Get a meal plan from the database
     """
     click.echo("Getting meal plan")
     meal_plan = get_user_meal_plans_as_json(uid, include_ingredients)
     click.echo(meal_plan)
+    click.echo("Finished search")
+
+
+@cli.command()
+@click.option(
+    "--uid",
+    default=1,
+    help="The user ID to get the meal plan for",
+)
+@click.argument(
+    "meal_choice",
+    default="b",
+)
+@click.option(
+    "--include-ingredients",
+    default=True,
+    help="Whether to include the ingredients in the meal plan",
+)
+@click.option(
+    "--include-nutrients",
+    default=True,
+    help="Whether to include the ingredients in the meal plan",
+)
+def get_meal(uid: int, include_ingredients: bool, include_nutrients: bool,meal_choice: str):
+    """
+    Get breakfast from the database
+    """
+    click.echo("Getting breakfast")
+    if meal_choice == "b":
+        meal = get_user_meal_info_json(uid, include_ingredients, include_nutrients,"Breakfast")
+    elif meal_choice == "l":
+        meal = get_user_meal_info_json(uid, include_ingredients, include_nutrients,"Lunch")
+    elif meal_choice == "d":
+        meal = get_user_meal_info_json(uid, include_ingredients, include_nutrients,"Dinner")
+    elif meal_choice == "s":
+        meal = get_user_meal_info_json(uid, include_ingredients, include_nutrients,"Snack")
+    else:
+        click.echo("Invalid meal choice")
+        return
+    click.echo(meal)
     click.echo("Finished search")
 
 
@@ -128,6 +171,37 @@ def reinsert_vectors():
     insert_all_vectors(foods)
     click.echo("Inserted new food vectors")
     click.echo("Finished search")
+
+
+@cli.command()
+@click.option(
+    "--uid",
+    default=1,
+    help="The user ID to get info for",
+)
+def store_plan(uid: int):
+    """
+    Store a plan in the database
+    """
+    click.echo("Storing plan")
+    store_diet_plan(uid)
+    click.echo("Finished storing plan")
+
+
+@cli.command()
+@click.option(
+    "--uid",
+    default=1,
+    help="The user ID to get info for",
+)
+def get_cached_plan(uid: int):
+    """
+    Get diet plan from Redis
+    """
+    click.echo("Getting plan")
+    plan = get_cached_plan_json(uid)
+    click.echo(plan)
+    click.echo("Finished getting plan")
 
 
 if __name__ == "__main__":
