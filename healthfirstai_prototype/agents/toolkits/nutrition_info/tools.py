793dcb166e8ab06fbf0b10ca1a20deb3e448603f
Lawrence Lim <ll4715@nyu.edu>
2023-07-29 22:07:17 +0800

    feat: Added skeleton for nutrition_info toolkit

--- healthfirstai_prototype/agents/toolkits/nutrition_info/tools.py
+++ healthfirstai_prototype/agents/toolkits/nutrition_info/tools.py
@@ -1,8 +1,40 @@
-"""Nutrition Tools
+"""Nutrition Info Tools
 
-Tools used in agents for the nutrition feature.
+Tools used in the nutrition lookup feature
 
 """
+from typing import Type
+from pydantic import BaseModel
+from langchain.tools import BaseTool
+from .utils import find_similar_foods
+from healthfirstai_prototype.enums.meal_enums import MealNames
+from .schemas import FindSimilarFoodsInput
+
+
+class FindSimilarFoodsTool(BaseTool):
+    """
+    Retrieve user information from the database.
+
+    Parameters:
+        user_id (int): The ID of the user.
+
+    Returns:
+        dict: A dictionary containing the personal and goal information of the user.
+    """
+
+    name = "get_user_info"
+    description = """
+        Useful when you want to check the user personal information and goal information.
+        You should enter the user id.
+        """
+    args_schema: Type[BaseModel] = FindSimilarFoodsInput
+
+    def _run(self, food_name: str, top_k: int):
+        return find_similar_foods(food_name, top_k)
+
+    def _arun(self, food_name: str, top_k: int):
+        raise NotImplementedError("get_user_info does not support async")
+
 
 # TODO: When I implement the SQL query tool, consider splitting up the tool by task. So, one of them can be
 # I think that it makes sense for me to write a custom LangChain agent
---
+++ healthfirstai_prototype/agents/toolkits/nutrition_info/utils.py
@@ -0,0 +1,14 @@
+"""Nutrition Info Utils
+
+Utility functions usesd in the nutrition lookup toolkit
+
+"""
+
+from healthfirstai_prototype.models.new_data_models import FoodInfo
+
+def find_similar_foods(food_name: str, top_k: int):
+    """
+    Find nutritionally similar foods to the given food
+
+    """
+    return
