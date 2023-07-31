045d7ac7580ccf2c4d7f108ea72310cd69d27b79
Yan <100032599+whykay-01@users.noreply.github.com>
2023-07-31 11:39:30 +0800

    chore: created empty skeleton to test functions

---
+++ tests/test_diet_plan_utils.py
@@ -0,0 +1,108 @@
+"""
+This file serves as a tester for the utilities for the diet plan agent.
+"""
+
+from healthfirstai_prototype.agents.toolkits.diet_plan.utils import (
+    create_new_custom_daily_meal_plan,
+    insert_into_relationship_table,
+    get_user_info_dict,
+    insert_into_personalized_daily_meal_plan,
+    cache_diet_plan_redis,
+    store_new_diet_plan,
+    store_meal,
+    get_cached_plan_json,
+    rank_tools,
+    format_nutrients_with_units,
+    create_nutrient_dict,
+    update_nutrient_values,
+    meal_to_nutrition_mapping,
+    get_user_meal_info_json,
+    get_food_by_ingredient_id,
+    get_diet_plan,
+    get_meal,
+    edit_entire_diet_plan,
+    edit_meal,
+    edit_diet_plan_json,
+)
+
+# NOTE: Since I was working only with a couple of these functions, I will test those first.
+
+
+def test_create_new_custom_daily_meal_plan():
+    pass
+
+
+def test_insert_into_relationship_table():
+    pass
+
+
+def test_get_user_info_dict():
+    pass
+
+
+def test_insert_into_personalized_daily_meal_plan():
+    pass
+
+
+def test_cache_diet_plan_redis():
+    pass
+
+
+def test_store_new_diet_plan():
+    pass
+
+
+def test_store_meal():
+    pass
+
+
+def test_get_cached_plan_json():
+    pass
+
+
+def test_rank_tools():
+    pass
+
+
+def test_format_nutrients_with_units():
+    pass
+
+
+def test_create_nutrient_dict():
+    pass
+
+
+def test_update_nutrient_values():
+    pass
+
+
+def test_meal_to_nutrition_mapping():
+    pass
+
+
+def test_get_user_meal_info_json():
+    pass
+
+
+def test_get_food_by_ingredient_id():
+    pass
+
+
+def test_get_diet_plan():
+    pass
+
+
+def test_get_meal():
+    pass
+
+
+def test_edit_entire_diet_plan():
+    pass
+
+
+def test_edit_meal():
+    pass
+
+
+def test_edit_diet_plan_json():
+    pass
